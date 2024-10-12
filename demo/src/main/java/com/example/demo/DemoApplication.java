316d5fed73a224ca2669fda678180db158ae014f
Fatima EL ASRI <fatimaelassri041@gmail.com>
2024-10-12 17:05:53 +0100

    test

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.register;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RegisterApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RegisterApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.register;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RegisterApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
